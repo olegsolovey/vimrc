173e7a1a12045e2f9ef9229da3cb083d48d7cdf8
Oleg Solovey <osolovey@snapchat.com>
2021-01-23 16:06:20 -0800

    install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# backup
+mv ~/.vim ~/.vim.orig
+mv ~/.vimrc ~/.vimrc.orig
+
+# clone the repo
+git clone https://github.com/olegsolovey/vimrc.git ~/.vim
+ln -s ~/.vim/vimrc ~/.vimrc
+
+# setup vundle
+git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
+
+vim -c 'PluginInstall' -c 'qa!'
